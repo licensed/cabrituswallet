649ab1ffc778c70fbb604760c9c2d67a142250f7
Ricardo Dantas <ricardilhodantas@gmail.com>
2024-07-16 16:10:09 -0300

    Initial Commit

---
+++ create.js
@@ -0,0 +1,18 @@
+const bip32 = require("bip32")
+const bip39 = require("bip39")
+const bitcoin = require("bitcoinjs-lib")
+const network = bitcoin.networks.testnet
+const path = `m/49'/1'/0'/0`
+let mnemonic = bip39.generateMnemonic()
+const seed = bip39.mnemonicToSeedSync(mnemonic)
+let root = bip32.fromSeed(seed, network)
+let account = root.derivePath(path)
+let node = account.derive(0).derive(0)
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address
+
+console.log("Carteira Gerada. Address: ", btcAddress)
+console.log("Chave Privada: ", node.toWIF())
+console.log("Seed", mnemonic)
